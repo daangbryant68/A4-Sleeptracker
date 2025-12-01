befd59e69029d5d1041244376fc599e9604b37ae
daang_bryant <bryantdang.68@gmail.com>
2025-12-01 06:10:35 -0800

    Home Page Changes

--- sleeptracker/src/app/home/home.page.ts
+++ sleeptracker/src/app/home/home.page.ts
@@ -1,27 +1,86 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { SleepService } from '../services/sleep.service';
-import { SleepData } from '../data/sleep-data';
-import { OvernightSleepData } from '../data/overnight-sleep-data';
-import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+	IonContent,
+	IonHeader,
+	IonTitle,
+	IonToolbar,
+	IonCard,
+	IonCardHeader,
+	IonCardTitle,
+	IonCardContent,
+	IonButton,
+	IonIcon,
+	IonText,
+	IonRow,
+	IonCol,
+	IonGrid, IonCardSubtitle
+} from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { bed, moon, statsChart, happy, sad, time } from 'ionicons/icons';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+	selector: 'app-home',
+	templateUrl: './home.page.html',
+	styleUrls: ['./home.page.scss'],
+	standalone: true,
+	imports: [
+		CommonModule,
+		FormsModule,
+		RouterModule,
+		IonContent,
+		IonHeader,
+		IonTitle,
+		IonToolbar,
+		IonCard,
+		IonCardHeader,
+		IonCardTitle,
+		IonCardContent,
+		IonButton,
+		IonIcon,
+		IonText,
+		IonRow,
+		IonCol,
+		IonGrid,
+		IonCardSubtitle
+	]
 })
-export class HomePage {
-  constructor(public sleepService:SleepService) {
+export class HomePage implements OnInit {
+	today = new Date();
+	greeting = '';// You could make this customizable
 
+	constructor() {
+		addIcons({ bed, moon, statsChart, happy, sad, time });
 	}
 
 	ngOnInit() {
-		console.log(this.allSleepData);
+		this.setGreeting();
 	}
 
-	/* Ionic doesn't allow bindings to static variables, so this getter can be used instead. */
-	get allSleepData() {
-		return SleepService.AllSleepData;
+	setGreeting() {
+		const hour = this.today.getHours();
+		if (hour < 12) {
+			this.greeting = 'Good Morning';
+		} else if (hour < 18) {
+			this.greeting = 'Good Afternoon';
+		} else {
+			this.greeting = 'Good Evening';
+		}
+	}
+
+	getDayOfWeek(): string {
+		const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+		return days[this.today.getDay()];
+	}
+
+	getFormattedDate(): string {
+		const options: Intl.DateTimeFormatOptions = {
+			month: 'long',
+			day: 'numeric',
+			year: 'numeric'
+		};
+		return this.today.toLocaleDateString('en-US', options);
 	}
 }
